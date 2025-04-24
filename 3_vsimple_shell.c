8614dcb1ce1c43d8e6eb0dcebdbe896f28fa3323
camtimms <cam.g.timms@gmail.com>
2025-04-24 07:13:14 +0000

    working on 3_vsimple_shell

---
+++ 3_vsimple_shell.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * main - Super Simple Shell
+ *
+ * Description: Super Simple Shell that can run commands with their full path,
+ * without any argument.
+ *
+ * Return: Success (0) or Failure (-1)
+ */
+
+int main (void)
+{
+	char *line =  NULL;
+	size_t len = 0;
+	int num_char = 0;
+	pid_t child_pid;
+	int status;
+
+	while (1)
+	{
+		printf("$ ");
+
+		num_char = getline(&line, &len, stdin);
+		if (num_char == -1)
+			break;
+
+		child_pid = fork();
+
+		if (child_pid == -1)
+		{
+			perror("fork failed:");
+			return (-1); 
+		}
+		if (child_pid == 0)
+		{
+			if (execve(line, NULL, NULL) == -1)
+			{
+				perror("execve failed:");
+				return (-1);
+			}
+		}
+		else
+			wait(&status);
+	}
+	
+	free (line);
+	return (0);
+}
