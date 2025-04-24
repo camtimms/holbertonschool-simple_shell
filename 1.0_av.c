5831d32858a025454a05d58cc07aa9a87d61e824
camtimms <cam.g.timms@gmail.com>
2025-04-24 03:42:08 +0000

    added 1.0_av

--- 1.0_av.c
+++ 1.0_av.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+/**
+ * main - Prints the arguments passed
+ *
+ * Return: Success (0)
+ */
+
+int main(int argc, char* argv[])
+{
+	int i = 0;
+
+	printf("Number of arguments: %d\n", argc);
+	
+	while (argv[i] != NULL)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
