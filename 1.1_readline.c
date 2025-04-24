0f664f61ce1b1b1e14508b03412de250c3a1c466
camtimms <cam.g.timms@gmail.com>
2025-04-24 04:16:47 +0000

    added 1.1_readline

---
+++ 1.1_readline.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the command the user inputs
+ *
+ * Return: Success (0)
+ */
+
+int main(void)
+{
+	char* line = NULL;
+	size_t len = 0;
+	int num_char = 0;
+
+	while (1)
+	{
+		printf("$ ");
+
+		num_char = getline(&line, &len, stdin);
+		if (num_char == -1)
+			break;
+
+		printf("%s", line);
+	}
+	
+	free(line);	
+	return (0);
+}
