e930fcdbc4092c92b60ad312520e426bac1e84c5
camtimms <cam.g.timms@gmail.com>
2025-04-29 07:06:14 +0000

    made tokenisation into helper function

---
+++ helper_functions.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * line_to_arr - Converts a string/line to an array
+ *
+ * @line: String/line to tokenise
+ *
+ * Description: Tokenises each word in a string and sets them to positions in
+ * an array
+ *
+ * Return: Array or failure (NULL)
+ */
+
+char **line_to_arr(char *line)
+{
+	int i = 0;
+	int arr_size = 2; 
+	char *token;
+	char **arr = malloc(arr_size * sizeof(char *));
+	char **temp;
+
+	if (arr == NULL)
+	{
+		perror("malloc failed");
+		return (NULL);
+	}
+
+	token = strtok(line, " ");
+	
+	while (token != NULL)
+	{
+		if (i >= arr_size - 1)
+		{
+			arr_size *= 2;
+			temp = realloc(arr, arr_size * sizeof(char *));
+			if (temp == NULL)
+			{
+				perror("realloc failed");
+				free(arr);
+				return (NULL);
+			}
+			arr = temp;
+		}
+
+		arr[i] = token;
+		token = strtok(NULL, " ");
+		i++;
+	}
+
+	arr[i] = NULL;
+	return (arr);
+}
