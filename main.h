bd64e25495782a52cd70f854a7ea3c6bd23d4839
Bevan Tan <btan400@gmail.com>
2025-04-30 03:53:47 +0000

    combined for checker

--- main.h
+++ main.h
@@ -6,9 +6,12 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <sys/stat.h>
 
 char *remove_space(char *);
 char *remove_traling_spaces(char *);
 char **line_to_arr(char *);
+char *get_path(char *);
 
 #endif
---
+++ simple_shell_test.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+/**
+ * main - Super Simple Shell
+ *
+ * Description: Super Simple Shell that can run commands with their full path,
+ * without any argument.
+ *
+ * Return: Success (0) or Failure (-1)
+ */
+
+int main(void)
+{
+	char *line =  NULL;
+	size_t len = 0;
+	int num_char = 0;
+	pid_t child_pid;
+	int status;
+	char **argv = NULL;
+	char *command_path;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("$ ");
+
+		num_char = getline(&line, &len, stdin);
+		if (num_char == -1)
+			break;
+
+		if (line[num_char - 1] == '\n')
+			line[num_char - 1] = '\0';
+
+		argv = line_to_arr(line);
+		if (argv[0] == NULL)
+			continue;
+
+		/*printf("current command going into get_path:%s\n", argv[0]);*/
+		command_path = get_path(argv[0]);
+		/*printf("command_path: %s\n", command_path);*/
+		/*command_path != executable we return to new $input*/
+		if (command_path == NULL)
+		{
+			continue;
+		}
+		child_pid = fork();
+
+		if (child_pid == -1)
+		{
+			perror("fork failed");
+			free(line);
+			return (-1);
+		}
+		if (child_pid == 0)
+		{
+			if (execve(command_path, argv, NULL) == -1)
+			{
+				perror("execve failed");
+				return (EXIT_FAILURE);
+			}
+		}
+		else
+			wait(&status);
+	}
+
+	free(line);
+	return (0);
+}
