a3b804f728fc1b688c153160b7ff0c26567cb1c0
camtimms <cam.g.timms@gmail.com>
2025-05-04 05:45:56 +0000

    finnaly reduced main down to less than 40 lines

--- simple_shell.c
+++ simple_shell.c
@@ -1,29 +1,36 @@
 #include "main.h"
 
 /**
- * custom_command - Handles the built in commands for the shell
+ * process_command - Handles the command input and returns the command path
  *
  * @arr_arg: Array of arguments from stdin
  * @exit_status: Pointer to the exit_status variable
+ * @line_count: Keeps track of the number of lines executed
+ * @argv: Program name and it's arguments
  *
- * Description: Handles the checks for the built in commands like exit and env
+ * Description: Handles the checks for the built in commands like exit and env.
+ * If the command is valid it then checks if it can find a path to the command
+ * from the environment and returns the command path if it does.
  *
- * Return: 1 to skip execution, 0 no custom command, -1 exits the shell
+ * Return: Command path on success or NULL on failure
  */
 
-int custom_command(char **arr_arg, int *exit_status)
+char *process_command(char **arr_arg, int *exit_status,
+			int *line_count, char **argv)
 {
 	int i = 0;
+	char *command_path;
 
 	/* Check if user entered empty command */
 	if (arr_arg[0] == NULL)
-		return (1);
+		return (NULL);
 
 	/* Check for exit command */
 	if (strcmp(arr_arg[0], "exit") == 0)
 	{
 		*exit_status = 0;
-		return (-1);
+		free_arr(arr_arg);
+		exit(0);
 	}
 
 	/* Check for env command */
@@ -32,10 +39,24 @@ int custom_command(char **arr_arg, int *exit_status)
 		for (i = 0; environ[i] != NULL; i++)
 			printf("%s\n", environ[i]);
 		*exit_status = 0;
-		return (1);
+		free_arr(arr_arg);
+		(*line_count)++;
+		return (NULL);
 	}
 
-	return (0);
+	/* Get file path for the command */
+	command_path = get_path(arr_arg[0]);
+	if (command_path == NULL)
+	{
+		fprintf(stderr, "%s: %d: %s: not found\n",
+			argv[0], *line_count, arr_arg[0]);
+		*exit_status = 127;
+		free_arr(arr_arg);
+		(*line_count)++;
+		return (NULL);
+	}
+
+	return (command_path);
 }
 
 
@@ -44,7 +65,7 @@ int custom_command(char **arr_arg, int *exit_status)
  *
  * @arr_arg: Array of arguments from stdin
  * @command_path: Path to the command
- * @exit_status Pointer to the exit_status variable
+ * @exit_status: Pointer to the exit_status variable
  *
  * Description: Executes the command given by command path in a child process
  *
@@ -99,66 +120,37 @@ int main(int argc, char **argv)
 	char **arr_arg = NULL;
 	int num_char = 0, line_count = 1, exit_status = 0;
 	size_t len = 0;
-	int cmd_status;
 	(void)argc;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			printf("$ ");
-
 		/* Pull line from stdin */
 		num_char = getline(&line, &len, stdin);
 		if (num_char == -1)
 			break;
-
 		/* Removed newline char from line */
 		if (line[num_char - 1] == '\n')
 			line[num_char - 1] = '\0';
-
-		/* Convert line to array */
+		/* Convert line into an array for each argument*/
 		arr_arg = line_to_arr(line);
 		if (arr_arg == NULL)
 		{
 			line_count++;
 			continue;
 		}
-
-		/*Check for custom command */
-		cmd_status = custom_command(arr_arg, &exit_status);
-		if (cmd_status == -1)
-		{
-			free_arr(arr_arg);
-			break;
-		}
-		if (cmd_status == 1)
-		{
-			free_arr(arr_arg);
-			line_count++;
-			continue;
-		}
-
-		/* Get file path for the command */
-		command_path = get_path(arr_arg[0]);
+		/* Process the array and find the path to the correct command */
+		command_path = process_command(arr_arg, &exit_status, &line_count, argv);
 		if (command_path == NULL)
-		{
-			fprintf(stderr, "%s: %d: %s: not found\n",
-				argv[0], line_count, arr_arg[0]);
-			exit_status = 127;
-			free_arr(arr_arg);
-			line_count++;
 			continue;
-		}
-
-		/* Fork process */
+		/* Execute the command by forking the process */
 		execute_command(arr_arg, command_path, &exit_status);
-
 		/* Free memory for next command */
 		free(command_path);
 		free_arr(arr_arg);
 		line_count++;
 	}
-
 	free(line);
 	return (exit_status);
 }
